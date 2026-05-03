d8de75f6400874360b2d727cde97348985808549
Tony <tony@gmail.com>
2026-05-04 05:22:48 +0800

    Fix single-quote to double-quote and add maven-publish.

--- Library/build.gradle.kts
+++ Library/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("maven-publish")
 }
 group = "com.permissionx.guolindev"
-version = "1.0.2"
+version = "1.0.0"
 
 android {
     namespace = "com.permissionx.guolindev"
@@ -24,6 +24,18 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"]) // 发布 release 版本的 AAR
+                groupId = "com.permissionx.guolindev"
+                artifactId = "Library"
+                version = "1.0.0"
+            }
+        }
     }
 }
 dependencies {
